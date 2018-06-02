6dc514c45ab02809831a5dbfccff12145082f384
zp1679827225 <1679827225@qq.com>
2018-06-03 00:36:21 +0800

    remind some errors

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -9,8 +9,11 @@ module.exports = async (ctx, next) => {
             queryObj = {}
             querystring = 'SELECT `*` FROM dishes'
         }else{
-            querystring = 'SELECT `dish_id`,`amount` FROM `order_record` WHERE `order_id` IN \
-            (SELECT `order_id` FROM `orders` WHERE `user_id1` = '+userid+')'
+            querystring = 'SELECT * FROM (SELECT `dish_id`,sum(`amount`) as `user_ordered_count` FROM `order_record` WHERE `order_id` IN \
+            (SELECT `order_id` FROM `orders` WHERE `user_id1` = '+userid+') \
+			GROUP BY `dish_id`) as tmp1\
+			LEFT JOIN \
+			(SELECT `dish_id`,`dish_name` as `name` FROM `dishes`) as tmp2 ON tmp1.`dish_id` = tmp2.`dish_id`'
         }
 		
         var results = await query.query(ctx,next,querystring, queryObj)
