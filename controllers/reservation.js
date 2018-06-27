b411174629da873e390758084be96ea743aac596
zp1679827225 <1679827225@qq.com>
2018-06-27 11:45:39 +0800

    remind some errors

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -11,16 +11,16 @@ module.exports = async (ctx, next) => {
 			if(!table_id){
 				throw new Error('table_id needed!')
 			} else {
-				sql1 = 'SELECT `user_id` FROM `table` WHERE `table_id` ='+table_id
+				sql1 = 'SELECT `user_id2` FROM `distribution` WHERE `table_id` ='+table_id
 				var result1 = await query.query(ctx,next,sql1,queryObj)
-				user_id0 = result1[0].user_id
+				user_id0 = result1[0].user_id2
 				if (!user_id0) {
-				sq12 = 'UPDATE `table` SET `user_id` = \''+user_id+'\' WHERE `table_id` ='+table_id
+				sq12 = 'UPDATE `distribution` SET `user_id2` = \''+user_id+'\' WHERE `table_id` ='+table_id
 				await query.query(ctx,next,sql2,queryObj)
 				sql3 = 'CREATE EVENT event_0 \
 				        ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 15 MINUTE \
 						DO \
-						    UPDATE `table` SET `user_id` = NULL WHERE `table_id` ='+table_id
+						    UPDATE `distribution` SET `user_id2` = NULL WHERE `table_id` ='+table_id
 				await query.query(ctx,next,sql3,queryObj)
 				} else {
 					throw new Error('table reserved or being used!')
