1082c50995e85ef1835168cc6c53c5ea4773dffd
zp1679827225 <1679827225@qq.com>
2018-06-30 22:14:49 +0800

    ...

--- controllers/review.js
+++ controllers/review.js
@@ -4,16 +4,17 @@ module.exports = async (ctx, next)=>{
 	star = info.star
 	queryObj = {}
     try{
-        	dish_id = ctx.params.dish_id
+      dish_id = ctx.params.dish_id
 			user_id = ctx.request.query.user_id
 			if(!dish_id){
 				throw new Error('dish_id needed!')
 			} else {
-				sql1 = 'SELECT `star`,`star_count` FROM `dishes` WHERE `dish_id` ='+dish_id
-				var result1 = await query.query(ctx,next,sql1,queryObj)
-				star_new = result1[0].star + star
-				star_count = star_count+1
-				sq12 = 'UPDATE `dishes` SET `star` = \''+star_new+'\', `star_count` = \''+star_count+'\' WHERE `dish_id` ='+dish_id
+				sql1 = 'SELECT `star_times`,`star_count` FROM `dishes` WHERE `dish_id` ='+dish_id
+				result1 = await query.query(ctx,next,sql1,queryObj)
+				star_count = result1[0].star_count+star
+				star_times = star_times+1
+        console.log(dish_id,star_times,star_count)
+				sq12 = 'UPDATE `dishes` SET `star_times` = \''+star_times+'\', `star_count` = \''+star_count+'\' WHERE `dish_id` ='+dish_id
 				await query.query(ctx,next,sql2,queryObj)
 			}
         }catch(e){
